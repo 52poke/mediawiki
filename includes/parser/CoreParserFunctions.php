dfec5b776a0e36c67e24078b6d8a26d841aac702
Mudkip <i@mudkip.me>
2022-12-25 18:04:05 +0000

    Fix filepath

--- includes/parser/CoreParserFunctions.php
+++ includes/parser/CoreParserFunctions.php
@@ -1124,7 +1124,7 @@ class CoreParserFunctions {
 					$url = wfExpandUrl( $mto->getUrl(), PROTO_RELATIVE );
 				}
 			}
-			$parser->mOutput->addImage( $file->title->getDBkey(), $file->getTimestamp(), $file->getSha1() );
+			$parser->getOutput()->addImage( $file->title->getDBkey(), $file->getTimestamp(), $file->getSha1() );
 			if ( $isNowiki ) {
 				return [ $url, 'nowiki' => true ];
 			}
